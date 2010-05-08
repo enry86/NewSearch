3bc6116788021463949fac3e2c9c6b4aee12b106
Enrico Sartori <sartori.enrico@gmail.com>
2010-05-08 16:45:35 +0200

    added indexer, preprocessor

---
+++ index/process.py
@@ -0,0 +1,57 @@
+'''
+Utilities for text preprocessing
+'''
+import stemmer
+import re
+
+def remove_tags (text):
+    reg = re.compile('<[ \]/:a-z]*>')
+    text, occ = re.subn(reg, '', text)
+    return text
+
+
+def remove_punc (text):
+    chars = '.,:;[]()_*\'\"<>{}=-+/\\_!|^&%#'
+    for c in chars:
+        if c in text:
+            text = text.replace(c, '')
+    return text
+   
+
+def read_sw_file (fn):
+    f = open(fn, 'r')
+    l = f.readlines()
+    f.close()
+    l = map(str.strip, l)
+    return l
+
+
+def remove_stopw (lst):
+    sw = read_sw_file('stopw.txt')
+    for w in lst:
+        if w in sw:
+            lst.remove(w)
+    
+
+def stemming (lst):    
+    ps = stemmer.PorterStemmer()
+    res = []
+    for w in lst:
+        w = str(w)
+        res.append(ps.stem(w, 0, len(w) - 1))
+    return res
+
+
+def preprocess (text):
+    text = text.lower()
+    text = remove_tags(text)
+    text = remove_punc(text)
+    lst = text.split()
+    print len(lst)
+    remove_stopw(lst)
+    print len(lst)
+    lst = stemming(lst)
+    print len(lst)
+    remove_stopw(lst)
+    return lst
+
