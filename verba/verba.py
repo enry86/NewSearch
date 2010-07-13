025c37ec2021f704c97d23c55cf86af53166d45a
Enrico Sartori <sartori.enrico@gmail.com>
2010-07-13 23:25:10 +0200

    verbs recognition prototype

--- verba/verba.py
+++ verba/verba.py
@@ -3,40 +3,51 @@
 import sys
 import os
 import getopt
-
+import verbs_finder
 from xml.dom import minidom
 
 def read_opts (argv):
     res = {}
-    res['in_dir'] = 'xml'
+    res['db'] = 'verbs.db'
+    try:
+        res['in_dir'] = argv[1].replace('/', '')
+    except:
+        res['in_dir'] = 'xml'
     return res
 
 
 def main ():
     conf = read_opts(sys.argv)
-    docs = read_files(conf['in_dir'])
+    docs = read_files(conf['in_dir'], conf['db'])
+    output_docs(docs)
     
 
-def read_files (xml_dir):
+def read_files (xml_dir, db):
     files = os.listdir(xml_dir)
     res = {}
     for f in files:
         d_id = f[:-4]
         cont = minidom.parse(xml_dir + '/'  + f)
+        doc = cont.getElementsByTagName('c:document')
+        text = get_text(doc)
         desc = cont.getElementsByTagName('rdf:Description')
-        ents = get_entities(desc)
-        res[d_id] = ents
-    return res
+        ents, pos = get_entities(desc)
+        rel = verbs_finder.get_relationship(text, pos, db)
+        res[d_id] = (ents, rel)
+    return res 
 
 
 def get_entities (ent_lst):
     doc = {}
+    pos = []
     for e in ent_lst:
         id_ent = get_entity_id(e)
         if id_ent != None:
             phrase = e.getElementsByTagName('c:exact')
             if phrase != []:
                 kw = get_keywords(phrase)
+                loc = get_location(e)
+                pos.append((loc, id_ent))
                 try:
                     doc[id_ent][0].append(kw)
                 except:
@@ -48,9 +59,21 @@ def get_entities (ent_lst):
                     doc[id_ent][1] = rel
                 except:
                     pass
-    return doc
+    pos.sort()
+    return doc, pos
 
-            
+
+def get_text (doc):
+    res = None
+    try:
+        node = doc[0].firstChild
+        res = node.nodeValue
+    except:
+        pass
+    return res
+
+
+
 def get_entity_id (e):
     res = None
     s = e.getElementsByTagName('c:subject')
@@ -73,12 +96,41 @@ def get_relevance (rel):
     return res
     
 
+def get_location (ent):
+    res = None
+    o_lst = ent.getElementsByTagName('c:offset')
+    l_lst = ent.getElementsByTagName('c:length')
+    try:
+        o_node = o_lst[0].firstChild
+        l_node = l_lst[0].firstChild
+        res = (int(o_node.nodeValue), int(l_node.nodeValue))
+    except:
+        pass
+    return res
+
+
+def output_docs (docs):
+    for d in docs:
+        ent, rel = docs[d]
+        for e in ent:
+            print e, 'Relevance:', ent[e][1]
+            for k in ent[e][0]:
+                print k
+            print '\n'
+        print '\n'
+        for r in rel:
+            for i in r[0]:
+                print i
+            print r[1]
+            print '\n'
+
+
+
+        
 
 
 
 
-def parse (xml):
-    cont = minidom.parse(xml)
 
 
 if __name__ == '__main__':
--- verba/verbs_finder.py
+++ verba/verbs_finder.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
 
-def get_relationship (text, pos):
+import sqlite3
+
+def get_relationship (text, pos, db):
     sent = select_sent(text, pos)
-  #  rels = constr_rel(sent)
+    rels = constr_rel(sent, db)
+    return rels
 
 
 def select_sent (text, pos):
@@ -35,11 +38,52 @@ def isolate_sent (text, base):
     return res, base + len(res)
 
 
-def constr_rel (sent):
+def constr_rel (sent, db):
+    res = []
     for sen, pos, ent_l in sent:
         base, end = pos
         for e in ent_l:
-            off, leng = e[0]
+            off, lng = e[0]
             off -= base
+            sen = sen.replace(sen[off : off + lng], '')
+            base += lng
+        verbs = retr_verbs(sen, db)
+        res.append((ent_l, verbs))
+    return res
 
+
+def retr_verbs (s, db):
+    res = []
+    s = s.replace('.',' ')
+    s = s.replace(',',' ')
+    s = s.replace(';',' ')
+    s = s.replace(':',' ')
+    kw = s.split()
+    
+    con = sqlite3.connect(db)
+    for k in kw:
+        if is_verb(k, con):
+            res.append(k)
+        else:
+            k = stem(k)
+            if is_verb(k, con):
+                res.append(k)
+    con.close()
+    return res
+            
+def stem (word):
+    res = None
+    if word.endswith('ing'):
+        res = word[:-3]
+    elif word.endswith('ed'):
+        res = word[:-2]
+    elif word.endswith('s'):
+        res = word[:-1]
+    return res
         
+
+def is_verb (word, con):
+    sql = 'select count(*) from verbs where verb like "%s"'
+    res = con.execute(sql % word)
+    cnt, = res.fetchone()
+    return cnt
