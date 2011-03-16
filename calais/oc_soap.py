fc519e82e11c32945fe02c14166e7fca97d309d5
Enrico Sartori <sartori.enrico@gmail.com>
2011-03-16 01:36:19 +0100

    new calais client

--- calais/oc_soap.py
+++ calais/oc_soap.py
@@ -10,6 +10,7 @@ Options:
     -r  directory of results
     -p  directory of read pages
     -k  OpenCalais key
+    -t  type of content [html, raw, xml]
 '''
 
 import utils.calais as calais
@@ -19,6 +20,19 @@ import getopt
 import pickle
 
 class CalaisClient:       
+    def __init__ (self, conf):
+        self.conf = conf
+        try:
+            self.files = os.listdir(conf['repo'])
+        except OSError:
+            print 'ERR: Invalid path repo'
+            sys.exit(3)
+        self.calais = calais.Calais(conf['key'], submitter='NewSearch')
+        self.calais.processing_directives['omitOutputtingOriginalText'] = 'false'
+        self.calais.processing_directives['contentType'] = conf['type']
+        
+
+
     def read_file (self, filename):
         tmp = ''
         f = open(filename, 'r')
@@ -29,39 +43,32 @@ class CalaisClient:
         return res
 
 
-    def write_file (self, filename, cont):
+    def write_file (self, filename, res):
         f = open(filename, 'w')
-        f.write(cont)
+        pickle.dump(res, f)
         f.close()
 
 
     def move_file (self, s_dir, d_dir, fname):
-        c = read_file(s_dir + '/' + fname)
-        write_file(d_dir + '/' + fname, c)
-        os.remove(s_dir + '/' + fname)
+        os.rename(s_dir + '/' + fname, d_dir + '/' + fname)
 
 
-    def call_srv (self, conf, cxml, files, srv):
+    def call_srv (self):
         try:
-            os.mkdir(conf['res'])
-            os.mkdir(conf['read'])
+            os.mkdir(self.conf['res'])
+            os.mkdir(self.conf['read'])
         except OSError:
             pass
-        for f in files:
-            cont = read_file(conf['repo'] + '/' + f)
-        res = srv.service.Enlighten(conf['key'], cont, cxml)
-        res_str = res.encode('utf8', 'ignore')
-        write_file(conf['res'] + '/' + f[:-4] + 'xml', res_str)
-        move_file(conf['repo'], conf['read'], f)
+        for f in self.files:
+            cont = self.read_file (self.conf['repo'] + '/' + f)
+            res = self.calais.analyze (cont, content_type = \
+                self.conf['type'], external_id = f)
+            self.write_file(self.conf['res'] + '/' + f[:-4] + 'pickle', res)
+            self.move_file(self.conf['repo'], self.conf['read'], f)
         
 
 def main ():
     conf = read_opts(sys.argv)
-    try:
-        files = os.listdir(conf['repo'])
-    except OSError:
-        print 'ERR: Invalid path repo'
-        sys.exit(3)
     cli = CalaisClient (conf)
     cli.call_srv ()
 
@@ -70,6 +77,7 @@ def read_opts (argv):
     res['read'] = 'pages_read'
     res['res'] = 'results'
     res['key'] = '33q562d5y52rqsxvfwm9s27e'
+    res['type'] = 'text/html'
     try:
         opts, args = getopt.gnu_getopt(argv, 'w:r:p:h')
     except getopt.GetoptError, err:
@@ -85,6 +93,8 @@ def read_opts (argv):
             res['read'] = v
         elif o == '-k':
             res['key'] = v
+        elif o == '-t':
+            res['type'] = 'text/' + v
     try:
         res['repo'] = argv[1]
     except IndexError:
