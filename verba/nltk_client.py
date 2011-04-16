ed801352c43434ed18371e51fc675392e0fb9479
Enrico Sartori <sartori.enrico@gmail.com>
2011-04-16 16:34:07 +0200

    wip

--- verba/nltk_client.py
+++ verba/nltk_client.py
@@ -15,7 +15,19 @@ class Extractor:
                 """
         self.pars = nltk.RegexpParser(self.gram)
         self.s_tok = nltk.data.load('tokenizers/punkt/english.pickle')
-
+        self.stopw = ['a','able','about','across','after','all','almost','also',\
+                          'am','among','an','and','any','are','as','at','be','because',\
+                          'been','but','by','can','cannot','could','dear','did','do',\
+                          'does','either','else','ever','every','for','from','get','got',\
+                          'had','has','have','he','her','hers','him','his','how','however',\
+                          'i','if','in','into','is','it','its','just','least','let','like',\
+                          'likely','may','me','might','most','must','my','neither','no',\
+                          'nor','not','of','off','often','on','only','or','other','our',\
+                          'own','rather','said','say','says','she','should','since','so',\
+                          'some','than','that','the','their','them','then','there','these',\
+                          'they','this','tis','to','too','twas','us','wants','was','we',\
+                          'were','what','when','where','which','while','who','whom','why',\
+                          'will','with','would','yet','you','your',"'s",',']
 
 
     '''
@@ -88,18 +100,19 @@ class Extractor:
                 elif t.node == 'NP':
                     np_tmp = self.read_tree (t)
                     if np_tmp:
-                        np.append (np_tmp)
+                        np += np_tmp
                 elif t.node == 'VP':
-                    verb = (self.read_tree (t))
+                    verb = ' '.join (self.read_tree (t))
         if verb or np:
             res.append ((verb, np))
         return res
 
 
     def read_tree (self, tree):
-        res = str ()
+        res = list ()
         for w in tree:
-            res += w[0] + ' '
+            if not w[0].lower () in self.stopw:
+                res.append (w[0])
         return res
 
 
