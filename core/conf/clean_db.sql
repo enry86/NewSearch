f44c3e9711b1d1fb4aa541a5ec1f654dd8c2292b
Enrico Sartori <sartori.enrico@gmail.com>
2011-05-19 15:23:04 +0200

    sync with svn

--- core/conf/clean_db.sql
+++ core/conf/clean_db.sql
@@ -1,13 +1,11 @@
 -- Cleanup of the database --
 
+DROP TABLE IF EXISTS `enrico`.`docs` ;
+DROP TABLE IF EXISTS `enrico`.`expansion` ;
 DROP TABLE IF EXISTS `enrico`.`triples` ;
 
-DROP TABLE IF EXISTS `enrico`.`docs` ;
+DROP TABLE IF EXISTS `enrico`.`keywords` ;
+DROP TABLE IF EXISTS `enrico`.`entities` ;
 
 DROP TABLE IF EXISTS `enrico`.`pages_index` ;
 
-DROP TABLE IF EXISTS `enrico`.`entities` ;
-
-DROP TABLE IF EXISTS `enrico`.`keywords` ;
-
-DROP TABLE IF EXISTS `enrico`.`expansion` ;
--- core/utils/database.py
+++ core/utils/database.py
@@ -1,62 +1,7 @@
 #!/usr/bin/python
 
-import sqlite3
 import MySQLdb
-
 import mysqlsettings
-
-
-class DataBaseSqlite:
-    __insert_ent = 'insert into entities values (NULL, ?)'
-    __lookup_ent = 'select id from entities where oc_id = ?'
-    __insert_kws = 'insert into keywords values (?,?)'
-
-    def __init__ (self, db_fn):
-        self.db = db_fn
-
-
-    def __start_connection(self):
-        res = True
-        try:
-            self.con = sqlite3.connect(self.db)
-        except Exception:
-            res = False
-        return res
-
-    def insert_ent (self, ent):
-        db_start = self.__start_connection ()
-        if db_start:
-            try:
-                self.con.execute (self.__insert_ent, ent)
-                self.con.commit ()
-                self.con.close ()
-            except sqlite3.IntegrityError:
-                pass
-        return db_start
-
-    def insert_kws (self, kws):
-        db_start = self.__start_connection ()
-        if db_start:
-            try:
-                self.con.execute (self.__insert_kws, kws)
-                self.con.commit ()
-                self.con.close ()
-            except sqlite3.IntegrityError:
-                pass
-        return db_start
-
-    def lookup_ent (self, ent):
-        res = -1
-        db_start = self.__start_connection()
-        if db_start:
-            res = self.con.execute(self.__lookup_ent, ent)
-            try:
-                res = res.fetchone()[0]
-            except TypeError:
-                res = -1
-            self.con.close()
-        return res
-
 
 class DataBaseMysql:
     __insert_ent = """insert into entities values (NULL, %s)"""
@@ -100,7 +45,7 @@ class DataBaseMysql:
                 self.cur.execute (self.__insert_pin, pin)
                 self.con.commit ()
                 self.cur.close ()
-            except MySQLdb.IntegrityError:
+            except MySQLdb.IntegrityError, m:
                 res = False
         else:
             res = False
