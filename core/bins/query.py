7c5e34bedecf694c0e067b39800e5fd17b58481f
Enrico Sartori <sartori.enrico@gmail.com>
2011-08-17 10:40:19 +0200

    introduced stopwords

--- core/bins/query.py
+++ core/bins/query.py
@@ -83,6 +83,21 @@ class QueryParser:
 
 
 class QueryManager:
+    stopw = ['a','able','about','across','after','all','almost','also',\
+                      'am','among','an','and','any','are','as','at','be','because',\
+                      'been','but','by','can','cannot','could','dear','did','do',\
+                      'does','either','else','ever','every','for','from','get','got',\
+                      'had','has','have','he','her','hers','him','his','how','however',\
+                      'i','if','in','into','is','it','its','just','least','let','like',\
+                      'likely','may','me','might','most','must','my','neither','no',\
+                      'nor','not','of','off','often','on','only','or','other','our',\
+                      'own','rather','said','say','says','she','should','since','so',\
+                      'some','than','that','the','their','them','then','there','these',\
+                      'they','this','tis','to','too','twas','us','wants','was','we',\
+                      'were','what','when','where','which','while','who','whom','why',\
+                      'will','with','would','yet','you','your',"'s",',']
+
+
 
     def __init__ (self, ind, test, hexa, db):
         #self.db = utils.database.DataBaseMysql ()
@@ -97,6 +112,7 @@ class QueryManager:
     def run_query (self, sq):
         start = time.time ()
         query = self.__analyze (sq)
+        print query
         end = time.time ()
         res = self.sim.query_similarity (query)
         return res, (end - start), len (query)
@@ -123,10 +139,11 @@ class QueryManager:
     def __build_query (self, pos):
         prs = QueryParser ()
         for w, t in pos:
-            if 'VB' in t:
-                prs.found_verb (w)
-            else:
-                prs.found_noun (w)
+            if w not in self.stopw:
+                if 'VB' in t:
+                    prs.found_verb (w)
+                else:
+                    prs.found_noun (w)
         prs.end_query ()
         query = prs.get_result ()
         return query
